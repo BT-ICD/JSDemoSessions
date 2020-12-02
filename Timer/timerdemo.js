e92e056a489e3ff28e56ea2b2e987530838af65f
Bhavin Trivedi <dearbhavin@hotmail.com>
2020-12-02 09:38:17 +0530

    Updated setTimeout example with arrow function

--- Timer/timerdemo.js
+++ Timer/timerdemo.js
@@ -1,4 +1,5 @@
 var timerId;
+
 function startTimer(){
    // timerId = setTimeout(displayTime, 5000);
    timerId = setInterval(displayTime, 1000);
@@ -12,5 +13,12 @@ function displayTime(){
     let dateTextbox = document.getElementById('dateTextbox');
     let currentDate = new Date();
     dateTextbox.value =currentDate;
-    
+  
+}
+function setTimeoutDemo(){
+    let dateTextbox1 = document.getElementById('dateTextbox1');
+    setTimeout(()=>{
+        let date = new Date();
+        dateTextbox1.value = date;
+    },5000);
 }
