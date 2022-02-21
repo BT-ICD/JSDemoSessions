90439255895d4f99dacb3019f97af37c250e98b8
Bhavin Trivedi <dearbhavin@hotmail.com>
2022-02-21 19:49:57 +0530

    Example of QuerySelector, ByTagNames

--- DOM_QuerySelector/dom_querySelector.js
+++ DOM_QuerySelector/dom_querySelector.js
@@ -1,3 +1,26 @@
-function querySelectorDemo(){
-    
+/**
+ * To clear all the input tag within a particular div tag
+ */
+function clearAll(){
+    let i=0;
+    let studentNameDiv = document.getElementById("studentName");
+    let inputs = studentNameDiv.getElementsByTagName('input');
+
+    console.log(inputs.length);
+    for(i=0;i<inputs.length;i++){
+        inputs[i].value="";
+    }
+}
+
+/**
+ * To fetch all the checkbox from a document.
+ * Check All
+ */
+function checkAll(){
+    let i=0;
+    let inputs = document.querySelectorAll('input[type=checkbox]');
+    console.log(inputs.length);
+    for(i=0;i<inputs.length;i++){
+        inputs[i].checked=true;
+    }
 }
