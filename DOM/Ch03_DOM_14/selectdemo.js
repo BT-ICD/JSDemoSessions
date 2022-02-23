818dbed732a576cabcf42092cdc9386f7cbb63e6
Bhavin Trivedi <dearbhavin@hotmail.com>
2022-02-23 18:51:08 +0530

    Updated multiselect demo for options

--- DOM/Ch03_DOM_14/selectdemo.js
+++ DOM/Ch03_DOM_14/selectdemo.js
@@ -1,3 +1,6 @@
+let soap = document.getElementById('soap');
+let selectedSoapButton = document.getElementById('selectedSoapButton');
+selectedSoapButton.addEventListener('click', getSelectedSoaps);
 function getSelected(){
     var selectEle = document.getElementById('browser');
     var result = document.getElementById('result');
@@ -9,4 +12,15 @@ function setSelected(){
     //If value is not defined
     //selectEle.value = 'Google Chrome';
     selectEle.value = '2';
+}
+function getSelectedSoaps(){
+    
+    console.log(soap.length);
+    let dataOptions = soap.options;    
+    for(let i=0;i<dataOptions.length;i++){
+        let opt = dataOptions[i];
+        if(opt.selected){
+            console.log(opt.value);
+        }
+    }
 }
