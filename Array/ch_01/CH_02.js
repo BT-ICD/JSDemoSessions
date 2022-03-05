c40f40268d4589303aa4c7f559d427d3863be421
Bhavin Trivedi <dearbhavin@hotmail.com>
2022-03-05 21:26:14 +0530

    Example on array

---
+++ Array/ch_01/CH_02.js
@@ -0,0 +1,11 @@
+let btnArr1 = document.getElementById('btnArr1');
+
+btnArr1.addEventListener('click', ()=>{
+    const shopping = ['bread', 'milk', 'cheese', 'chocolates', 'noodles'];
+    console.log(shopping);
+    //length - to get number of elements of an array
+    console.log(shopping.length);
+    //To get value for individual element (position)
+    console.log(shopping[0]);
+    
+});
