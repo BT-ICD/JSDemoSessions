2c2b1d9c7fbbda743ea9267bb08fbe3fce5aa064
Bhavin Trivedi <dearbhavin@hotmail.com>
2022-03-06 18:38:49 +0530

    Example of array methods

--- Array/ch_01/CH_02.js
+++ Array/ch_01/CH_02.js
@@ -1,3 +1,7 @@
+/**
+ * Learning reference: 
+ * https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Arrays
+ */
 let btnArr1 = document.getElementById('btnArr1');
 let btnArr2 = document.getElementById('btnArr2');
 let btnArr3= document.getElementById('btnArr3');
@@ -6,7 +10,11 @@ let btnArr5= document.getElementById('btnArr5');
 let btnArr6= document.getElementById('btnArr6');
 let btnArr7= document.getElementById('btnArr7');
 let btnArr8= document.getElementById('btnArr8');
-
+let btnArr9= document.getElementById('btnArr9');
+let btnArr10= document.getElementById('btnArr10');
+let btnArr11= document.getElementById('btnArr11');
+let btnArr12= document.getElementById('btnArr12');
+let btnArr13= document.getElementById('btnArr13');
 
 btnArr1.addEventListener('click', ()=>{
     const shopping = ['bread', 'milk', 'cheese', 'chocolates', 'noodles'];
@@ -77,3 +85,89 @@ btnArr3.addEventListener('click',()=>{
     console.log(cities);     // [ "Manchester" ]
     console.log(cities.length);
   });
+
+  /**
+   * To remove the first item from an array, use shift()
+   */
+  btnArr7.addEventListener('click',()=>{
+    const cities = ['Manchester', 'Liverpool'];
+    console.log(cities);
+    console.log(cities.length);
+    cities.shift();
+    console.log(cities);     // [ "Liverpool" ]
+    console.log(cities.length);
+  });
+
+  /**
+   * To remove item from a particular position/index, use splice()
+   */
+  btnArr8.addEventListener('click',()=>{
+    const cities = ['Manchester', 'Liverpool', 'Edinburgh', 'Carlisle'];
+    console.log(cities);
+    console.log(cities.length);
+    let removedElements = cities.splice(1,1); //index, number of elements
+    console.log(cities); // ['Manchester', 'Edinburgh', 'Carlisle']
+    console.log(cities.length); 
+    console.log(removedElements); // [ 'Liverpool']
+
+  });
+
+  /**
+   * To access every element of an array. Statement for ... of
+   */
+   btnArr9.addEventListener('click',()=>{
+    const birds = ['Parrot', 'Falcon', 'Owl'];
+    for(const bird of birds){
+        console.log(bird);
+    }
+   });
+
+   /**
+    * To get new array from existing array with modified values map()
+    */
+   btnArr10.addEventListener('click',()=>{
+    const numbers = [5, 2, 7, 6];
+    const doubled = numbers.map(double);
+    console.log(numbers);
+    console.log(doubled);
+   });
+
+   function double(number){
+       return number*2;
+   }
+
+   /**
+    * To filter elements of an array, use filter()
+    */
+   btnArr11.addEventListener('click',()=>{
+    const cities = ['London', 'Liverpool', 'Totnes', 'Edinburgh'];
+    const longer = cities.filter(isLong);
+    console.log(cities); // ['London', 'Liverpool', 'Totnes', 'Edinburgh']
+    console.log(longer);  // [ "Liverpool", "Edinburgh" ]
+   });
+
+   function isLong(city){
+       return city.length>8;
+   }
+
+   /**
+    * Converting from string to an array, use split()
+    */
+    btnArr12.addEventListener('click',()=>{
+    const data = 'Manchester,London,Liverpool,Birmingham,Leeds,Carlisle';
+    const cities = data.split(',');
+    console.log(data);
+    console.log(cities);
+   });
+
+   /**
+    * Converting from an array to string, use join() or toString()
+    */
+    btnArr13.addEventListener('click',()=>{
+        const countries = ["India","Japan","USA","Canada", "France"] ;
+        const countrieStr = countries.join(',');
+        const countrieStr1 = countries.toString();
+        console.log(countries);
+        console.log(countrieStr);
+        console.log(countrieStr1);
+    });
