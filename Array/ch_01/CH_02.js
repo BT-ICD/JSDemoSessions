a1e17526a0320bb6658ac01b5c4844662fcae31c
Bhavin Trivedi <dearbhavin@hotmail.com>
2022-03-06 09:57:18 +0530

    Methods of array example

--- Array/ch_01/CH_02.js
+++ Array/ch_01/CH_02.js
@@ -1,4 +1,12 @@
 let btnArr1 = document.getElementById('btnArr1');
+let btnArr2 = document.getElementById('btnArr2');
+let btnArr3= document.getElementById('btnArr3');
+let btnArr4= document.getElementById('btnArr4');
+let btnArr5= document.getElementById('btnArr5');
+let btnArr6= document.getElementById('btnArr6');
+let btnArr7= document.getElementById('btnArr7');
+let btnArr8= document.getElementById('btnArr8');
+
 
 btnArr1.addEventListener('click', ()=>{
     const shopping = ['bread', 'milk', 'cheese', 'chocolates', 'noodles'];
@@ -9,3 +17,63 @@ btnArr1.addEventListener('click', ()=>{
     console.log(shopping[0]);
     
 });
+
+/**
+ * Multideminsional Array - Array which contains array
+ */
+btnArr2.addEventListener('click',()=>{
+    const random = ['tree', 795, [0, 1, 2]];
+    console.log(random);
+    console.log(random[2]);
+    console.log(random[2][0]);
+    console.log(random[2][1]);
+
+});
+
+/**
+ * Finding items in an array
+ */
+btnArr3.addEventListener('click',()=>{
+    const birds = ['Parrot', 'Falcon', 'Owl'];
+    console.log(birds.indexOf('Owl'));   //  2
+    console.log(birds.indexOf('Rabbit')); // -1
+    
+});
+/**
+ * Adding items to array - using push method - To add one or more items to the end of an array we can use push() method.
+ */
+ btnArr4.addEventListener('click',()=>{
+    const cities = ['Manchester', 'Liverpool'];
+    console.log('Length of array is ', cities.length);
+    cities.push('Cardiff');
+    console.log(cities);      // [ "Manchester", "Liverpool", "Cardiff" ]
+    console.log('Length of array is ', cities.length);
+    cities.push('Bradford', 'Brighton');
+    console.log(cities);      // [ "Manchester", "Liverpool", "Cardiff", "Bradford", "Brighton" ]
+    console.log('Length of array is ', cities.length);
+    
+ });
+/**
+ * Adding items to the start of the array, use unshift()
+ */
+ btnArr5.addEventListener('click',()=>{
+    const cities = ['Manchester', 'Liverpool'];
+    console.log(cities);     // ["Manchester", "Liverpool" ]
+    console.log(cities.length); 
+    cities.unshift('Edinburgh');
+    console.log(cities);     // [ "Edinburgh", "Manchester", "Liverpool" ]
+    console.log(cities.length); 
+    
+ });
+
+ /**
+  * To remove the last item from the array, use pop()
+  */
+  btnArr6.addEventListener('click', ()=>{
+    const cities = ['Manchester', 'Liverpool'];
+    console.log(cities);     // [ "Manchester",'Liverpool' ]
+    console.log(cities.length);
+    cities.pop();
+    console.log(cities);     // [ "Manchester" ]
+    console.log(cities.length);
+  });
