bf85536250a844873ca7129ab4543de8611a5bdb
Bhavin Trivedi <dearbhavin@hotmail.com>
2022-05-10 15:09:42 +0530

    Updated Destructuring assignment

--- ObjectDestructuring/destructuring.js
+++ ObjectDestructuring/destructuring.js
@@ -1,6 +1,15 @@
+/**
+ * About Destructuring assignment
+ * Reference:
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+ */
 let btn1 = document.getElementById('btn1');
 let btn2 = document.getElementById('btn2');
+let btn3 = document.getElementById('btn3');
+let btn4 = document.getElementById('btn4');
+let btn5 = document.getElementById('btn5');
 
+//To extract member of Math class
 const {PI, E} = Math;
     console.log(PI);
     console.log(E);
@@ -10,6 +19,7 @@ btn1.addEventListener('click',()=>{
     console.log(circleArea(circle));
 });
 
+//Destructuring from array elements
 btn2.addEventListener('click',()=>{
     //Destructuring array element (values) into variables
     const [first,second,,forth]= [10,20,30,40];
@@ -24,10 +34,6 @@ btn2.addEventListener('click',()=>{
     const[firstItem, ...restItems]=[10,20,30,40,50];
     console.log(firstItem);
     console.log(restItems);
-
-
-
-
 });
 //Destructuring as a part of argument of the function
 
@@ -41,4 +47,44 @@ const circle={
 //function to find area of circle. destructured radius from circle object
 //Function expects object with radius as a property of an object
 const circleArea =({radius})=> (PI*radius*radius).toFixed(2);
+//Step - 3
+//Assignment separate from declaration
+btn3.addEventListener('click',()=>{
+let a,b ;
+[a,b]=[1,2];
+console.log(a);
+console.log(b);
+});
 
+/**
+ * Swapping variables
+ * Two variables values can be swapped in one destructuring expression.
+ */
+btn4.addEventListener('click',()=>{
+    let a = 1;
+    let b = 3;
+
+    console.log(a); // 1
+    console.log(b); // 3
+
+    [a, b] = [b, a];
+    console.log(a); // 3
+    console.log(b); // 1
+});
+
+/**
+ * Object destructuring
+ * Basic assignment
+ */
+ btn5.addEventListener('click',()=>{
+    const user = {
+        id: 42,
+        isVerified: true
+    };
+    
+    const {id, isVerified} = user;
+    
+    console.log(id); // 42
+    console.log(isVerified); // true
+    
+ });
