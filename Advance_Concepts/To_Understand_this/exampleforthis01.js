228fa493e2e5f845cfde1b59aa1033c8a12ae607
Bhavin <61580782+BT-ICD@users.noreply.github.com>
2022-12-22 21:31:24 +0530

    behavior of this object with event handler

---
+++ Advance_Concepts/To_Understand_this/exampleforthis01.js
@@ -0,0 +1,9 @@
+let btn2 = document.getElementById('btn2');
+btn2.addEventListener('click',btn2Click)
+function btn1Click(){
+    console.log(this);
+}
+
+function btn2Click(){
+    console.log(this);
+}
