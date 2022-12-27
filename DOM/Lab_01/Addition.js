ca701b2eb5f34b7d4230df841ac397fa35763720
Bhavin <61580782+BT-ICD@users.noreply.github.com>
2022-12-27 21:57:53 +0530

    updated with lab: opposite and addition

---
+++ DOM/Lab_01/Addition.js
@@ -0,0 +1,16 @@
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+function addition(){
+    let txtVal1 = document.getElementById('txtVal1');
+    let txtVal2 = document.getElementById('txtVal2');
+    let txtAnswer = document.getElementById('txtAnswer');
+    txtAnswer.value = parseInt(txtVal1.value)+ parseInt( txtVal2.value);
+
+}
+function clear(){
+    let txtVal1 = document.getElementById('txtVal1');
+    let txtVal2 = document.getElementById('txtVal2');
+    let txtAnswer = document.getElementById('txtAnswer');
+    txtVal1.value='';
+    txtVal2.value='';
+    txtAnswer.value=''; 
+}
---
+++ DOM/Lab_01/oppositeNumber.js
@@ -0,0 +1,7 @@
+function displayOpposite(){
+
+    let txtInput = document.getElementById('txtInput');
+    let txtOutput = document.getElementById('txtOutput');
+    txtOutput.value= txtInput.value *-1;
+
+}
