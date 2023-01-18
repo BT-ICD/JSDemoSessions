8048bbc57884e1dbd45a5276cbfdf9b24be248ac
Bhavin <61580782+BT-ICD@users.noreply.github.com>
2023-01-18 08:55:57 +0530

    Switch example updated

--- SwitchCaseDemo/switchcasedemo.js
+++ SwitchCaseDemo/switchcasedemo.js
@@ -7,25 +7,32 @@ function getResult() {
     let n = parseInt(weekdaynum.value);
     switch (n) {
         case 1:
-            result.value = "Monday"
+            result.value = "Monday";
+            console.log("Monday");
             break;
         case 2:
-            result.value = "Tuesday"
+            result.value = "Tuesday";
+            console.log("Tuesday");
             break;
         case 3:
-            result.value = "Wednesday"
+            result.value = "Wednesday";
+            console.log("Wednesday");
             break;
         case 4:
-            result.value = "Thursday"
+            result.value = "Thursday";
+            console.log("Thursday");
             break;
         case 5:
-            result.value = "Friday"
+            result.value = "Friday";
+            console.log("Friday");
             break;
         case 6:
-            result.value = "Saturday"
+            result.value = "Saturday";
+            console.log("Saturday");
             break;
         case 7:
-            result.value = "Sunday"
+            result.value = "Sunday";
+            console.log("Sunday");
             break;
         default:
             result.value = "Please enter value between 1 and 7";
