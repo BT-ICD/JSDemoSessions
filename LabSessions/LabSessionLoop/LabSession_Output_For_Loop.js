199bd2c82bedb93423a2e77d2e47b9108ef1e6ed
Bhavin <61580782+BT-ICD@users.noreply.github.com>
2023-01-29 18:52:14 +0530

    Example to understand Output of nested loop

---
+++ LabSessions/LabSessionLoop/LabSession_Output_For_Loop.js
@@ -0,0 +1,38 @@
+function outputexample1(){
+    let i,j;
+    for(i=1;i<=4;i++){
+        for(j=1;j<=3;j++){
+            console.log(j)
+        }
+    }
+}
+function outputexample2(){
+    let i,j;
+    for(i=1;i<=4;i++){
+        for(j=1;j<=3;j++){
+            console.log("i="+i+" and  j="+j);
+        }
+    }
+}
+function outputexample3(){
+    let i,j;
+    let data='';
+    for(i=1;i<=4;i++){
+        data=''
+        for(j=1;j<=4;j++){
+            data=data+' ' + i;    
+        }
+        console.log(data)
+    }
+}
+function outputexample4(){
+    let i,j;
+    let data='';
+    for(i=1;i<=4;i++){
+        data=''
+        for(j=1;j<=i;j++){
+            data=data+' ' + j;    
+        }
+        console.log(data)
+    }
+}
