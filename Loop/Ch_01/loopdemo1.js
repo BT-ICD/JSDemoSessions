ededccd16f246dfadd2dd0281a2f51dc1673e3c1
BT-ICD <icd.bhavin@gmail.com>
2024-02-12 19:51:25 +0530

    Loop demo exercise to determine output

---
+++ Loop/Ch_01/loopdemo1.js
@@ -0,0 +1,18 @@
+function demo1(){
+    let i;
+    console.log('Loop Demo 1 ');
+    // for(i=1;i<=5;i++){
+    //     console.log(i);
+    // }
+
+    // for(i=1;i<=5;i=i+2){
+    //     console.log(i);
+    // }
+
+    for(i=10;i<=5;i=i+3){
+        console.log(i);
+    }
+
+}
+
+
