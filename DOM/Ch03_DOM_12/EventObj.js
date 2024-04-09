d7e6ce86ed1b9c93313608cacd007b7e3742de93
BT-ICD <icd.bhavin@gmail.com>
2024-04-09 19:17:07 +0530

    event object example

---
+++ DOM/Ch03_DOM_12/EventObj.js
@@ -0,0 +1,22 @@
+function buttonClick(e){
+    console.log('Hello from button clicked');
+    console.log(e);
+}
+function onKeyDown(e){
+    console.log('Hello from key down event ');
+    // console.log(e);
+    
+    // console.log(e.isTrusted);
+    // console.log(e.altKey);
+    
+    // if(e.keyCode>=48  &&e.keyCode<=57){
+
+    // }
+    // else{
+    //     event.preventDefault();
+    // }
+}
+function valueChange(e1){
+console.log('change event');
+console.log(e1);
+}
